bc8558d6d75ea76ef487532f075726b691dbf3ac
LaOngDaoxing <18731197300@163.com>
2020-11-12 17:39:29 +0800

    修改注释

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,18 +4,18 @@ import { promiseRequestVar } from "../../request/promiseRequest.js";
 Page({
   data: {
     // 轮播图数组
-    swiperList: [],
+    carouselList: [],
     // 导航 数组
-    catesList:[],
+    navsList:[],
     // 楼层数据
     floorList:[]
   },
   // 页面开始加载 就会触发
   onLoad: function (options) {
     // 一、使用原生请求wx.request({});调用后台接口，获取轮播图数据
-    // this.useMulitWxRequestInOneFunMayBugToGetSwiperList();
+    // this.useMulitWxRequestInOneFunMayBugToGetSlideshowList();
     // 二、使用请求Promise((resolve,reject)=>{wx.request({});})调用后台接口，获取轮播图数据 
-    this.usePromiseWxRequestToGetSwiperList();
+    this.usePromiseWxRequestToGetSlideshowList();
     // this.getCateList();
     // this.getFloorList();
       
@@ -25,7 +25,7 @@ Page({
   * @Bug
       注意，套娃使用原生请求wx.request({});可能会出现异步回调地狱。
    */
-  useMulitWxRequestInOneFunMayBugToGetSwiperList(){
+  useMulitWxRequestInOneFunMayBugToGetSlideshowList(){
     // 1 发送异步请求获取轮播图数据  优化的手段可以通过es6的 promise来解决这个问题 
     wx.request({
       /**
@@ -53,7 +53,7 @@ Page({
       success: (result) => {
         console.log(result);
         this.setData({
-          swiperList: result.data.slideshowList
+          carouselList: result.data.slideshowList
         })
       }
     });
@@ -61,20 +61,20 @@ Page({
   /**
    * @Description：二、使用请求Promise((resolve,reject)=>{wx.request({});})调用后台接口，获取轮播图数据 
    */
-  usePromiseWxRequestToGetSwiperList(){
+  usePromiseWxRequestToGetSlideshowList(){
     promiseRequestVar({ url: "/slideshowData" })
     .then(result => {
       this.setData({
-        swiperList: result
+        carouselList: result
       })
     })
   },
   // 获取 分类导航数据
-  getCateList(){
+  getNavigationsList(){
     promiseRequestVar({ url: "/home/catitems" })
     .then(result => {
       this.setData({
-        catesList: result
+        navsList: result
       })
     })
   },
