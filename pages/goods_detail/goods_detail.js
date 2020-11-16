34d39fdc654f25496842f03bde5091e6cc3f689f
LaOngDaoxing <18731197300@163.com>
2020-11-16 16:25:37 +0800

    点击轮播图 放大预览

--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -45,6 +45,8 @@ Page({
   },
   /**
    * @Description：获取商品详情数据
+   * @ResponseData:
+      {"message": {"goods_id": 8888,"cat_id": 1085,"goods_name": "spike 经典武士大马士革直刀(微型) 户外野营直刀 收藏礼品刀 饰品刀具","goods_price": 500,"goods_number": 100,"goods_weight": 100,"goods_introduce": "富文本内容","goods_state": 2,"is_del": "0","add_time": 1516361489,"upd_time": 1516361489,"delete_time": null,"hot_mumber": 0,"is_promote": false,"cat_one_id": 995,"cat_two_id": 1075,"cat_three_id": 1085,"goods_cat": "995,1075,1085","pics": [{"pics_id": 38711,"goods_id": 8888,"pics_big": "http://image1.suning.cn/uimg/b2c/newcatentries/0070134290-000000000149003877_1_800x800.jpg","pics_mid": "http://image1.suning.cn/uimg/b2c/newcatentries/0070134290-000000000149003877_1_400x400.jpg","pics_sma": "http://image1.suning.cn/uimg/b2c/newcatentries/0070134290-000000000149003877_1_200x200.jpg","pics_big_url": "http://image1.suning.cn/uimg/b2c/newcatentries/0070134290-000000000149003877_1_800x800.jpg","pics_mid_url": "http://image1.suning.cn/uimg/b2c/newcatentries/0070134290-000000000149003877_1_400x400.jpg","pics_sma_url": "http://image1.suning.cn/uimg/b2c/newcatentries/0070134290-000000000149003877_1_200x200.jpg"}],"attrs": [{"goods_id": 8888,"attr_id": 9210,"attr_value": "户外直刀","add_price": 0,"attr_name": "主体参数-类别","attr_sel": "only","attr_write": "manual","attr_vals": "放大镜"}]},"meta": {"msg": "获取成功","status": 200}}
    */
   async getGoodsDetail(goods_id) {
     const goodsObj = await promiseRequestVar2({ url: "/goods/detail", data: { goods_id } });
@@ -54,12 +56,11 @@ Page({
     // 2 判断当前商品是否被收藏
     let isCollect = collect.some(v => v.goods_id === this.GoodsInfo.goods_id);
     this.setData({
+      // 对象解构赋值：必须优化动态渲染数据goodsObj，以加快页面加载速度
       goodsObj: {
         goods_name: goodsObj.goods_name,
         goods_price: goodsObj.goods_price,
-        // iphone部分手机 不识别 webp图片格式 
-        // 最好找到后台 让他进行修改 
-        // 临时自己改 确保后台存在 1.webp => 1.jpg 
+        // iphone部分手机 不识别 webp图片格式；最好找到后台 让他进行修改；临时自己改 确保后台存在 1.webp => 1.jpg。
         goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
         pics: goodsObj.pics
       },
@@ -68,6 +69,8 @@ Page({
   },
   /**
    * @Description：点击轮播图 放大预览
+   * @Reference：
+      微信官方文档•小程序》切换 API》媒体》图片》wx.previewImage》
    */
   handlePrevewImage(e) {
     // 1 先构造要预览的图片数组 
--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -81,6 +81,8 @@ Page({
   },
   /**
    * @Description：获取商品列表数据
+   * @ResponseData:
+      {"message":{"total":10,"pagenum":1,"goods":[{"goods_id":57445,"cat_id":9,"goods_name":"创维（Skyworth）65V9E65英寸25核4KHDR高清智能电视","goods_price":6499,"goods_number":100,"goods_weight":100,"goods_big_logo":"","goods_small_logo":"","add_time":1516663280,"upd_time":1516663280,"hot_mumber":0,"is_promote":false,"cat_one_id":1,"cat_two_id":3,"cat_three_id":9}]},"meta":{"msg":"获取成功","status":200}}
    */
   async getGoodsList(){
     const res=await promiseRequestVar2({url:"/goods/search",data:this.QueryParams});
